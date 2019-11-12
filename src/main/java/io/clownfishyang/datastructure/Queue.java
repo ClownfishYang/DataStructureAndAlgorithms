1cebf6f082d1811fd180e3813145fdc2132e7f4c
ClownfishYang <ClownfishYang@163.com>
2019-11-12 16:57:23 +0800

    modify-gitignore

---
+++ src/main/java/io/clownfishyang/datastructure/Queue.java
@@ -0,0 +1,11 @@
+package io.clownfishyang.datastructure;
+
+/**
+ * Copyright (C), 2015-2019, 深圳市环球易购电子商务有限公司<br>
+ * $END$<br>
+ *
+ * @author ClownfishYang<br>
+ * created on 2019/11/4 10:54<br>
+ */
+public class Queue {
+}
